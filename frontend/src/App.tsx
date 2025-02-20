a0a3843a4748480f059604cfe6515bd83add6441
muhanangmahrub <muhanangmahrub@gmail.com>
2025-02-20 11:57:02 +0700

    added header and body

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { defaultSystem } from "@chakra-ui/react";
 import Header from "./components/Header";
+import TextField from "./components/Body";
 
 function App() {
   return (
     <ChakraProvider value={defaultSystem}>
       <Header />
+      <TextField />
     </ChakraProvider>
   );
 }
